40189ffc7790cb52bad9c25c9bf1687afb97ccc1
eajdrogE <egordjae@gmail.com>
2024-10-05 12:23:40 +0300

    добавлена проверка на повтор символы в алфавите

--- src/main/java/org/example/DataCarrier/DataInput.java
+++ src/main/java/org/example/DataCarrier/DataInput.java
@@ -2,6 +2,8 @@ package org.example.DataCarrier;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
 public class DataInput {
     public static String alphabet;
     public static String inputText;
@@ -12,7 +14,7 @@ public class DataInput {
             // Чтение файла с алфавитом
             alphabet = new String(Files.readAllBytes(Paths.get("alphabet.txt")));
             System.out.println("Alphabet: " + alphabet);
-
+            validateAlphabet(alphabet);
             // Чтение файла с открытым текстом / шифртекстом
             inputText = new String(Files.readAllBytes(Paths.get("in.txt")));
             System.out.println("Input Text: " + inputText);
@@ -23,5 +25,23 @@ public class DataInput {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    private static void validateAlphabet(String alphabet) {
+        if (alphabet == null || alphabet.isEmpty()) {
+            throw new IllegalArgumentException("Алфавит не может быть пустым.");
+        }
+        if (hasDuplicateCharacters(alphabet)) {
+            throw new IllegalArgumentException("Алфавит не должен содержать повторяющихся символов.");
+        }
+    }
+
+    private static boolean hasDuplicateCharacters(String str) {
+        Set<Character> charSet = new HashSet<>();
+        for (char c : str.toCharArray()) {
+            if (!charSet.add(c)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
